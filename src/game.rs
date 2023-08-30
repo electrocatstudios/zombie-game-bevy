9aa015dfeedffb90e368daf9d3c3b9b2b9b445f0
filtoid <philjeffes@hotmail.com>
2023-08-30 17:22:45 +0200

    rotate to face the correct way

--- src/game.rs
+++ src/game.rs
@@ -2,14 +2,14 @@ use bevy::{app::AppExit, prelude::*,sprite::MaterialMesh2dBundle};
 use std::path::Path;
 
 use super::{despawn_screen,MainGameState,menu::MenuState};
-
+use std::vec::Vec;
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app
             .add_systems(OnEnter(MainGameState::Game), game_setup)
-            .add_systems(Update, (menu_return_check,game_update).run_if(in_state(MainGameState::Game)))
+            .add_systems(Update, (menu_return_check,game_update,zombie_mover).run_if(in_state(MainGameState::Game)))
             .add_systems(OnExit(MainGameState::Game), despawn_screen::<OnGameScreen>);
     }
 }
@@ -26,6 +26,18 @@ struct AnimationIndices {
 #[derive(Component, Deref, DerefMut)]
 struct AnimationTimer(Timer);
 
+#[derive(Component)]
+struct Player {
+    loc: Vec2,
+}
+
+#[derive(Component)]
+struct Zombie {
+    loc: Vec::<Vec2>,
+    cur_loc: usize
+}
+
+
 fn game_setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
@@ -35,28 +47,71 @@ fn game_setup(
         .spawn(Camera2dBundle::default())
         .insert(OnGameScreen);
     
-    let texture_path = Path::new("images").join("zombie").join("zombie.png");
-    let texture_handle = asset_server.load(texture_path);
-    let texture_atlas =
-        TextureAtlas::from_grid(texture_handle, Vec2::new(200.0, 200.0), 4, 1, None, None);
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
-    let animation_indices = AnimationIndices { first: 0, last: 3 };
-    commands.spawn((
-        SpriteSheetBundle {
-            texture_atlas: texture_atlas_handle,
-            sprite: TextureAtlasSprite::new(animation_indices.first),
-            transform: Transform::from_scale(Vec3::splat(0.5)),
-            ..default()
-        },
-        animation_indices,
-        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
-    ))
-    .insert(OnGameScreen);
+    //Zombie
+    {
+        let texture_path = Path::new("images").join("zombie").join("zombie.png");
+        let texture_handle = asset_server.load(texture_path);
+        let texture_atlas =
+            TextureAtlas::from_grid(texture_handle, Vec2::new(200.0, 200.0), 4, 1, None, None);
+        let texture_atlas_handle = texture_atlases.add(texture_atlas);
+        let animation_indices = AnimationIndices { first: 0, last: 3 };
+        
+        let mut locations = Vec::<Vec2>::new();
+        locations.push(Vec2::new(-300.0, -200.0));
+        // locations.push(Vec2::new(-350.0, 0.0)); // This doesn't work with the angle 
+        locations.push(Vec2::new(-300.0, 200.0));
+        locations.push(Vec2::new(300.0, 200.0));
+        locations.push(Vec2::new(300.0, -200.0));
+        
+        commands.spawn((
+            SpriteSheetBundle {
+                texture_atlas: texture_atlas_handle,
+                sprite: TextureAtlasSprite::new(animation_indices.first),
+                transform: Transform::from_xyz(locations[0].x, locations[0].y, 0.0).with_scale(Vec3::splat(0.5)),
+                ..default()
+            },
+            animation_indices,
+            AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+        ))
+        .insert(Zombie{
+            loc: locations,
+            cur_loc: 0
+        })
+        .insert(OnGameScreen);
+    
+    }
+    
+    // Player
+    {
+        let texture_path = Path::new("images").join("player").join("player.png");
+        let texture_handle = asset_server.load(texture_path);
+        let texture_atlas =
+            TextureAtlas::from_grid(texture_handle, Vec2::new(200.0, 200.0), 4, 1, None, None);
+        let texture_atlas_handle = texture_atlases.add(texture_atlas);
+        let animation_indices = AnimationIndices { first: 0, last: 3 };
+
+        commands.spawn((
+            SpriteSheetBundle {
+                texture_atlas: texture_atlas_handle,
+                sprite: TextureAtlasSprite::new(animation_indices.first),
+                transform: Transform::from_xyz(80.0, 50.0, 0.0).with_scale(Vec3::splat(0.5)),
+                ..default()
+            },
+            animation_indices,
+            AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+        ))
+        .insert(Player{
+            loc: Vec2::new(80.0,50.0)
+        })
+        .insert(OnGameScreen);
+
+    }
+
+
 }
 
 fn game_update(
     time: Res<Time>,
-    _game_state: ResMut<NextState<MainGameState>>,
     mut query: Query<(
         &AnimationIndices,
         &mut AnimationTimer,
@@ -73,6 +128,86 @@ fn game_update(
             };
         }
     }
+}
+
+const ZOMBIE_SPEED: f32 = 50.0;
+
+fn zombie_mover(
+    time: Res<Time>,
+    mut zombies: Query<(
+        &mut Zombie,
+        &mut Transform,
+    )>,
+){
+    for (mut zombie, mut transform) in &mut zombies {
+        let cur_loc = zombie.loc[zombie.cur_loc];
+        let mut x_met = false;
+
+        if cur_loc.x != transform.translation.x {
+            if (cur_loc.x - transform.translation.x).abs() <= ZOMBIE_SPEED * time.delta_seconds() {
+                transform.translation.x = cur_loc.x;
+                x_met = true;
+            }else{
+                if transform.translation.x > cur_loc.x {
+                    transform.translation.x -= ZOMBIE_SPEED * time.delta_seconds();
+                }else{
+                    transform.translation.x += ZOMBIE_SPEED * time.delta_seconds();
+                }
+            }
+        }else{
+            x_met = true;
+        }
+
+        let mut y_met = false;
+
+        if cur_loc.y != transform.translation.y {
+            if (cur_loc.y - transform.translation.y).abs() <= ZOMBIE_SPEED * time.delta_seconds() {
+                transform.translation.y = cur_loc.y;
+                y_met = true;
+            }else{
+                if transform.translation.y > cur_loc.y {
+                    transform.translation.y -= ZOMBIE_SPEED * time.delta_seconds();
+                }else{
+                    transform.translation.y += ZOMBIE_SPEED * time.delta_seconds();
+                }
+            }
+        }else{
+            y_met = true;
+        }
+
+        if x_met && y_met {
+            zombie.cur_loc += 1;
+            if zombie.cur_loc >= zombie.loc.len() {
+                zombie.cur_loc = 0;
+            }
+        }
+
+        // Calculate rotation
+        let pos = transform.translation.truncate();
+        let target = Vec2::new(zombie.loc[zombie.cur_loc].x, zombie.loc[zombie.cur_loc].y);
+        let diff = target - pos;
+        
+        let mut angle = 0.0;
+        if diff.y == 0.0 {
+            if diff.x > 0.0 {
+                angle = std::f32::consts::PI * 0.5;
+            } else {
+                angle = std::f32::consts::PI * 1.5;
+            }
+        } else if diff.x == 0.0 {
+            if diff.y > 0.0 {
+                angle = std::f32::consts::PI;
+            } else {
+                angle = 0.0;
+            }
+        }else {
+            // TODO: Fix this - it doesn't work at the moment
+            angle = (diff.y/diff.x).tan() + std::f32::consts::PI;
+            println!("pos {:?}, target: {:?}, diff: {:?}, angle: {:?} ", pos, target, diff, angle);
+        }
+        
+        transform.rotation = Quat::from_rotation_z(angle);
+    }    
 }
 
 fn menu_return_check(
